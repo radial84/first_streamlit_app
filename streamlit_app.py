c08573491257e6b96a64b43766dc9708d709fa43
radial84 <127948565+radial84@users.noreply.github.com>
2023-04-05 10:11:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # creates a df from json
 streamlit.dataframe(fruityvice_normalized)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
